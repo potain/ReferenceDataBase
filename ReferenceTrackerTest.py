0db4809a369d562358e5b9c61a7e8ebf650ba0e0
jingxiao00 <wangbomicro@gmail.com>
2016-12-25 11:39:35 +0100

    Add unitteste module ReferenceTrackerTeste

---
+++ ReferenceTrackerTest.py
@@ -0,0 +1,12 @@
+"""Unit Test for Reference Tracker
+
+..:: moduleauthors:: WangBo <wangbomicro@gmail.com>
+"""
+
+import unittest
+
+class ReferenceTrackerTest(unittest.TestCase):
+    """Unit Test for Reference Tracker"""
+    def setUp(self):
+        pass
+    
