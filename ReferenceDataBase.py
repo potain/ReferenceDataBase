40d51f20be76e3604ca679c3f4b298e39ef37bb8
unknown <wangbo@ICT0104033.imec.be>
2017-01-02 01:03:57 +0100

    ReferenceDataBase: Implemented the defined methods.

--- ReferenceDataBase.py
+++ ReferenceDataBase.py
@@ -3,8 +3,13 @@
 ..:: moduleauthor: Wang Bo <wangbomicro@gmail.com>
 ..:: invar: The ReferenceDataBase must have proper publications.
 """
+from Exceptions import IllegalValueException, IllegalAuthorsException, \
+    IllegalPublicationIdException
+import re
 
 class ReferenceDataBase(object):
+    
+    incrementID = 1001
     
     def __init__(self):
         """Initialize this new ReferenceDatabase with no publications attached 
@@ -15,18 +20,22 @@ class ReferenceDataBase(object):
             No publications are attached to this ReferenceDataBase.
         """
         ReferenceDataBase.incrementID = 1001
-        
+        self._isTerminated = False
+        self._publications = dict()        
+    
     def isTerminated(self):
         """Check whether this DataBase is already terminated.
         """
-        pass
+        return self._isTerminated
     
     def terminate(self):
         """Terminate this DataBase.
          Post: This DataBase is terminated.
          Post: All publication belonging to this DataBase have been Removed.
         """
-        pass
+        if not self.isTerminated():
+            self._publications.clear()
+        self._isTerminated = True
     
     def hasPublication(self, publication):
         """Check whether this DataBase has the given publication as one of the 
@@ -35,7 +44,7 @@ class ReferenceDataBase(object):
         Args:
             publication (publication): The publication to check.
         """
-        pass
+        return publication.getId() in self._publications
     
     def hasPublicationID(self, id):
         """Check whether publication with the given ID in the database.
@@ -47,8 +56,10 @@ class ReferenceDataBase(object):
             (boolean): True if the publication with given in the dataBase, 
             otherwise return false.    
         """
-        pass
-    
+        if not self.hasPublicationID(id):
+            raise IllegalPublicationIdException(id)
+        return self._publications[id];
+        
     def getPublicationWithID(self, id):
         """Get the publication in the database by the given ID.
         
@@ -62,7 +73,11 @@ class ReferenceDataBase(object):
             IllegalPublicationIdException: If the given ID do not exist in 
             the publication DataBase.
         """
-        pass
+        if not self.hasPublicationID(id):
+            raise IllegalPublicationIdException("The given publicationID is not \
+            valid")
+        
+        return self._publications[id];
     
     def hasProperPublication(self):
         """Check whether this DataBase has proper publications associated 
@@ -72,9 +87,12 @@ class ReferenceDataBase(object):
             (boolean): True if and only if this DataBase can have each of its 
             publications as a element of its publications set.
         """
-        pass
+        for publication in self.getAllPublications():
+            if not self.canHaveAsPublication(publication):
+                return False
+        return True
     
-    def canHaveAsPublication(self):
+    def canHaveAsPublication(self, publication):
         """Check whether this Database can have the given publication as one 
         of its elements.
      
@@ -84,7 +102,7 @@ class ReferenceDataBase(object):
         Return:
         (Boolean) False if the given publication is not effective.
         """
-        pass
+        return publication != None and not publication.isTerminated()
     
     def addAsPublication(self, publication):
         """Add the given publication to the set of publications attached to 
@@ -99,7 +117,15 @@ class ReferenceDataBase(object):
             to the DataBase. The given publication can not be attached to the 
             DataBase.
         """
-        pass
+        if not self.canHaveAsPublication(publication) or \
+        self.hasPublication(publication):
+            raise IllegalValueException("The database can not have the given\
+            publication.")
+        
+        publication.id = ReferenceDataBase.incrementID 
+        ReferenceDataBase.incrementID += 1
+        self._publications[publication.getId()] = publication
+
     
     def removePublication(self, publication):
         """Remove the given publication from the set of publications attached 
@@ -114,14 +140,18 @@ class ReferenceDataBase(object):
             publications, the given publication terminated, the association 
             between this publication and all the other publication are removed.
         """
-        pass
+        if self.hasPublication(publication): 
+            publication.terminate()
+            self._publications.remove(publication.getId())
+     
     
     def getAllPublication(self):
         """Return a set collecting all publications associated with this Database.
         """
-        pass
+        return self._publications.values()
     
-    def isValidAuthor(self):
+    @staticmethod
+    def isValidAuthor(author):
         """Check if the single authors name is valid. The valid name of an 
         author is given as initialOfFirstName. lastName, e.g., “A. Einstein”.
         
@@ -131,9 +161,10 @@ class ReferenceDataBase(object):
         Returns:
             (boolean): true if this is a valid author name.
         """
-        pass
+        return re.match('^[a-zA-Z ]{1,20}, [a-zA-Z][a-zA-Z ]{1,20}$', author) \
+            is not None
     
-    def findByAuthor(self):
+    def findByAuthor(self, authorName):
         """Find all publications authored by an author.
         
         Args:
@@ -145,7 +176,14 @@ class ReferenceDataBase(object):
             IllegalArgumentException If the given authorName is not in format 
             "initialOfFirstName.lastName"
         """
-        pass
+        if not self.isValidAuthor(authorName):
+            raise IllegalAuthorsException(authorName)
+        
+        referenceSet = set();
+        for publication in self.getAllPublications():
+            if authorName in publication.authorsNames:
+                referenceSet.add(publication)
+        return referenceSet;
         
     def findByTitleWord(self, word):
         """Returns all publications that have a given word in their title;
@@ -155,7 +193,11 @@ class ReferenceDataBase(object):
         Returns:
             (Set): Set of publications that have a given word in their title
         """
-        pass
+        referenceSet = set()
+        for publication in self.getAllPublications():
+            if word.lower() in publication.getTitle().lower():
+                referenceSet.add(publication)
+        return referenceSet;
     
     def addCitation(self, publicationId1, publicationId2):
         """Add a citation relationship of two publications. Given as a pair of 
@@ -169,7 +211,9 @@ class ReferenceDataBase(object):
             IllegalArgumentException: The publication with publicationID is 
             not in the DataBase The publication1 can not cites publication2.
         """
-        pass
+        publication1 = self.getPublicationWithID(publicationId1)
+        publication2 = self.getPublicationWithID(publicationId2)
+        publication1.addAsCites(publication2)
     
     @classmethod
     def getCurrentIncrementID(cls):
@@ -178,20 +222,18 @@ class ReferenceDataBase(object):
         Returns: 
             (int) The current increment ID.
         """
-        pass
+        return ReferenceDataBase.incrementID
     
     @classmethod
-    def setCurrentIncrementID(cls):
+    def setCurrentIncrementID(cls, ID):
         """Set the IncrementID to the given value.
         Args:
             ID (int): The id number to be set.
         Throws: 
             IllegalIncrementIDException: if the given ID is out of bound.
         """    
-        pass
+        ReferenceDataBase.incrementID = ID
     
-        
-    
-    
+
     
         
