786c5f69c1959c31411535fee97f10376a67396a
potain <presensor@gmail.com>
2017-01-03 00:31:47 +0100

    ConferencePaperTest: added test_getter__.

--- ConferencePaperTest.py
+++ ConferencePaperTest.py
@@ -15,3 +15,8 @@ class ConferencePaperTest(unittest.TestCase):
                                 self._authors, 1990, 'conference1');
         self.ConferencePaper2 = ConferencePaper("Gas leak rate study of MEMS", 
                                 self._authors, 2015, 'conference12');
+                                
+    def test__getter__(self):
+        self.assertEqual("Gas leak rate study of MEMS", self.ConferencePaper1.title)
+        self.assertEqual(self._authors, self.ConferencePaper1.authors)
+        self.assertEqual("conference1", self.ConferencePaper1.conference)
